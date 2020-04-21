4b6462ed25954b57cfdf16c5731630dbec377a4c
Bernard <bernardfinbarrs@yahoo.com>
2020-04-21 15:21:21 +0100

    Updates

---
+++ precache-manifest.ebad1cdc148a9ce37663fe125f29100c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b009360a152d84a405456a7ea7bc5a0b",
+    "url": "/social-card/index.html"
+  },
+  {
+    "revision": "a4b911e1d8a5c87639d8",
+    "url": "/social-card/static/js/2.d55dbe47.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/social-card/static/js/2.d55dbe47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a62180a3a417a204a985",
+    "url": "/social-card/static/js/main.3b8c0b7a.chunk.js"
+  },
+  {
+    "revision": "b0bb6c8bca042abb2164",
+    "url": "/social-card/static/js/runtime-main.7044ad6d.js"
+  }
+]);
